2c02084f48bdee4cd468a4a159d6e4c5913f8c10
Benjabc <bdbravo@miuandes.cl>
2019-03-28 19:07:38 -0300

    update from seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,4 +12,10 @@ events=Event.create([{name:"cumple tutu", descripcion:"Tutuween fiesta de disfra
 
 clients=Client.create([{name:"Benjamin",apellido:"Bravo",mail:"bdbravo@miuandes.cl",phone:93312007,pass:"qwerty",dir:"parque lauca norte 3392"},{name:"Paz",apellido:"Torres",mail:"cptorres@miuandes.cl",phone:93272014,pass:"123qwe",dir:"antupiren 1212"},{name:"Maria Jesus",apellido:"Torrealba",mail:"mjtorrealba@miuandes.cl",phone:99872124,pass:"password",dir:"camino san antonio 2392"},{name:"Bruno",apellido:"Alandia",mail:"bgalandia@miuandes.cl",phone:7422551,pass:"bruno123",dir:"manquehue 352"},{name:"Cristobal",apellido:"Uslar",mail:"cuslar@miuandes.cl",phone:933407337,pass:"gaston123",dir:"noruega 192"}])
 
+categories=Category.create([{type:"VIP"},{type:"Cancha"},{type:"Platea"}])
+
+tickets=Ticket.create([{name:"comple tutu",price:1500},{name:"petstatic",price:2500},{name:"hackaton",price:100}])
+
+ticketorder=OrderTicket.create([{total_payed:1700,client_id:clients.first},{total_payed:1700,client_id:clients.second},{total_payed:1700,client_id:clients.third},{total_payed:2700,client_id:clients.first},{total_payed:2700,client_id:clients.second},{total_payed:2700,client_id:clients.third},{total_payed:200,client_id:clients.first},{total_payed:200,client_id:clients.second},{total_payed:200,client_id:clients.third}])
+
 
